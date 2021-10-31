6dcc2395887ab1fe5ea2d41e076f1bd59198cb64
Christian Possidonio <christian@orientme.com.br>
2021-10-31 13:58:35 -0300

    Navbar navigating between routes

---
+++ src/pages/Graphics/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Heading, Flex } from '@chakra-ui/react'
+
+const Graphics: React.FC = (): JSX.Element => {
+    return (
+        <Flex alignItems="center" direction="column">
+            <Heading mb={10}>Graphics</Heading>
+        </Flex>
+    )
+}
+
+export default Graphics
---
+++ src/pages/Revenues/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Heading, Flex } from '@chakra-ui/react'
+
+const Revenues: React.FC = (): JSX.Element => {
+    return (
+        <Flex alignItems="center" direction="column">
+            <Heading mb={10}>Revenues</Heading>
+        </Flex>
+    )
+}
+
+export default Revenues
