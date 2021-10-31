fdeaa1b3874095085016d5171a4b4985940ab6c7
Christian Possidonio <christian@orientme.com.br>
2021-10-31 13:58:14 -0300

    Navbar navigating between routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ChakraProvider, Container } from '@chakra-ui/react'
-import Home from './pages/Home'
 import theme from './theme'
 import './theme/styles.css'
+import Navbar from './components/Navbar'
+import DarkModeToggle from './components/DarkModeToggle'
+import Investments from './pages/Investments'
+import Revenues from './pages/Revenues/index'
+import Payments from './pages/Payments'
+import Graphics from './pages/Graphics/index'
+import Dashboard from './pages/Dashboard/index'
 
 const App: React.FC = (): JSX.Element => {
     return (
         <ChakraProvider theme={theme}>
-            <Container maxWidth="container.xl">
+            <Container maxWidth="container.xl" padding={10}>
                 <Router>
-                    <div>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <Link to="/home">Home</Link>
-                                </li>
-                            </ul>
-                        </nav>
+                    <Navbar />
+                    <DarkModeToggle />
+                    <Switch>
+                        <Route exact path="/">
+                            <Dashboard />
+                        </Route>
 
-                        <Switch>
-                            <Route path="/home">
-                                <Home />
-                            </Route>
-                        </Switch>
-                    </div>
+                        <Route path="/investments">
+                            <Investments />
+                        </Route>
+                        <Route path="/revenues">
+                            <Revenues />
+                        </Route>
+                        <Route path="/payments">
+                            <Payments />
+                        </Route>
+                        <Route path="/graphics">
+                            <Graphics />
+                        </Route>
+                    </Switch>
                 </Router>
             </Container>
         </ChakraProvider>
